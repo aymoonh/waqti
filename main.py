14776e8e52c48a6b2826c7af16978fe2fdc72ff4
Ayman Hussein <aymanh@mne.gov>
2026-06-27 03:36:54 +0300

    fix: optional static directory mount

--- main.py
+++ main.py
@@ -34,7 +34,8 @@ except:
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+if os.path.isdir("static"):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 if os.path.isdir("uploads"):
     app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
